f732845e22ed8fef0af74b86dff251279ca80b81
suzuk12345 <tatakaka.0301.ini@gmail.com>
2023-10-25 20:30:22 +0900

    step1完成

---
+++ Deck.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'Card.php';
+
+class Deck
+{
+    private $deck = [];
+
+    // コンストラクタで暗黙的にデッキを構築
+    public function __construct()
+    {
+        $card = new Card();
+        $this->deck = $card->buildDeck();
+    }
+
+    // 一枚カードを引く
+    public function drawACard()
+    {
+        $drawnCard = array_pop($this->deck);
+        return $drawnCard;
+    }
+
+    // デッキの情報を取得
+    public function getDeck()
+    {
+        return $this->deck;
+    }
+}
