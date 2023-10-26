cbf979c22427f056370be3a172842a0ac36d3cab
suzuk12345 <tatakaka.0301.ini@gmail.com>
2023-10-26 10:39:35 +0900

    メソッドの移動/Aを1もしくは11として扱う処理を追加

--- Player.php
+++ Player.php
@@ -2,8 +2,9 @@
 class Player
 {
     protected $name = 'あなた';
-    private $hands = [];
-    private $score = 0;
+    protected $hands = [];
+    protected $score = 0;
+    protected $aceCount = 0;
 
     // プレイヤーの名前を取得
     public function getName()
@@ -26,7 +27,52 @@ class Player
     // 引いたカード(1枚)を手札に加える&得点を記録する
     public function addCardAndScore($drawnCard)
     {
+        if ($drawnCard[1] === 'A') {
+            $this->aceCount++;
+        }
         $this->hands[] = $drawnCard;
         $this->score += $drawnCard[2];
+        if ($this->aceCount >= 1 && $this->score >= 22) {
+            $this->score -= 10;
+            $this->aceCount--;
+        }
+    }
+
+    // 手札の最後の一枚を表示
+    public function displayLastHand($name, $hands)
+    {
+        $lastHand = array_slice($hands, -1, 1);
+        echo "{$name}の引いたカードは".$lastHand[0][0].'の'.$lastHand[0][1].'です。'.PHP_EOL;
+    }
+
+    // プレイヤー:得点が20以下の場合Hit or Stand
+    public function hitOrStand($player, $deck)
+    {
+        for ($i = $player->score; $i <= 20; $i = $player->score) {
+            $stand = false;
+            if ($player->aceCount >= 1) {
+                $soft = $player->score - 10;
+                echo "あなたの現在の得点は{$player->score}({$soft})です。カードを引きますか？（Y/N）";
+            } else {
+                echo "あなたの現在の得点は{$player->score}です。カードを引きますか？（Y/N）";
+            }
+
+            $input = trim(fgets(STDIN));
+
+            switch ($input) {
+                case 'Y':
+                    $player->addCardAndScore($deck->drawACard());
+                    $player->displayLastHand($player->name, $player->hands);
+                    break;
+                case 'N':
+                    $stand = true;
+                    break;
+                default:
+                    break;
+            }
+            if ($stand === true) {
+                break;
+            }
+        }
     }
 }
